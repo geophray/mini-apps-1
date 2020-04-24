aedc4a0b5a19f43a4dd637bbca1fe27c1555ce71
Jeff Maughan <geophray@gmail.com>
2020-04-24 15:29:48 -0700

    Move game state into game object

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,20 +5,36 @@ console.log('hello');
 //=================================================================
 var renderBoard = function () {
   // Output the current state of the game to the board.
-  for (let i = 0; i < gameState.length; i++) {
-    let currentRow = gameState[i];
+  for (let i = 0; i < game.board.length; i++) {
+    let currentRow = game.board[i];
     for (let j = 0; j < currentRow.length; j++) {
       document.getElementById('r' + i + 'c' + j).innerText = currentRow[j];
     }
   }
 }
 
+var renderScoreBoard = function () {
+  // For outputing score board info
+}
+
+
 //=================================================================
 // Model - stores the state of the game/board
 //=================================================================
-var gameState;
-var nextMove;
-var gameOver = false;
+var game = {
+  board: [
+    ['','',''],
+    ['','',''],
+    ['','','']
+  ],
+  nextMove: 'X',
+  gameOver: false,
+  players: {
+    X: '',
+    O: ''
+  }
+
+}
 
 var checkBoard = function () {
   // This will check all the rows, columns, and diagonals to see if someone has won
@@ -29,96 +45,98 @@ var checkBoard = function () {
 }
 
 var checkRows = function () {
-  for (let i = 0; i < gameState.length; i++) {
-    if (gameState[i][0] && gameState[i][0] === gameState[i][1] && gameState[i][0] === gameState[i][2]) {
-      gameOver = true;
-      setTimeout(() => {alert(gameState[i][0] + ' wins!')}, 100);
+  for (let i = 0; i < game.board.length; i++) {
+    if (game.board[i][0] && game.board[i][0] === game.board[i][1] && game.board[i][0] === game.board[i][2]) {
+      game.gameOver = true;
+      setTimeout(() => {alert(game.board[i][0] + ' wins!')}, 100);
     }
   }
 }
 
 var checkCols = function () {
-  for (let i = 0; i < gameState.length; i++) {
-    if (gameState[0][i] && gameState[0][i] === gameState[1][i] && gameState[0][i] === gameState[2][i]) {
-      gameOver = true;
-      setTimeout(() => {alert(gameState[0][i] + ' wins!')}, 100);
+  for (let i = 0; i < game.board.length; i++) {
+    if (game.board[0][i] && game.board[0][i] === game.board[1][i] && game.board[0][i] === game.board[2][i]) {
+      game.gameOver = true;
+      setTimeout(() => {alert(game.board[0][i] + ' wins!')}, 100);
     }
   }
 }
 
 var checkDiags = function () {
-  if (gameState[0][0] && gameState[0][0] === gameState[1][1] && gameState[0][0] === gameState[2][2]) {
-    gameOver = true;
-    setTimeout(() => {alert(gameState[0][0] + ' wins!')}, 100);
+  if (game.board[0][0] && game.board[0][0] === game.board[1][1] && game.board[0][0] === game.board[2][2]) {
+    game.gameOver = true;
+    setTimeout(() => {alert(game.board[0][0] + ' wins!')}, 100);
   }
-  if (gameState[0][2] && gameState[0][2] === gameState[1][1] && gameState[0][2] === gameState[2][0]) {
-    gameOver = true;
-    setTimeout(() => {alert(gameState[0][2] + ' wins!')}, 100);
+  if (game.board[0][2] && game.board[0][2] === game.board[1][1] && game.board[0][2] === game.board[2][0]) {
+    game.gameOver = true;
+    setTimeout(() => {alert(game.board[0][2] + ' wins!')}, 100);
   }
 }
 
 var isBoardFull = function () {
-  if (!gameOver) {
-    for (let i = 0; i < gameState.length; i++) {
-      let currentRow = gameState[i];
+  if (!game.gameOver) {
+    for (let i = 0; i < game.board.length; i++) {
+      let currentRow = game.board[i];
       for (let j = 0; j < currentRow.length; j++) {
         if (currentRow[j] === '') {
           return false;
         }
       }
     }
-    gameOver = true;
+    game.gameOver = true;
     setTimeout(() => {alert('Looks like a tie game! Clear the board to play again.')}, 100);
   }
 }
 
-
-//=================================================================
-// Controller - handles the game logic
-//=================================================================
-
-// Add event listeners
-document.getElementById("board").addEventListener("click", (e) => addNextMove(e));
-document.getElementById("reset").addEventListener("click", (e) => resetGame());
-
 // Function to reset the gameboard
 var resetGame = function () {
-  gameState = [
+  game.board = [
     ['','',''],
     ['','',''],
     ['','','']
   ];
-  nextMove = 'X';
-  gameOver = false;
+  game.gameOver = false;
   renderBoard();
 }
 
 // Initialize an empty board
 resetGame();
 
+//=================================================================
+// Controller - handles the game logic
+//=================================================================
 
-var addNextMove = function(e) {
-  if (e.target.textContent === '' && !gameOver) {
+// Add event listeners
+document.getElementById("board").addEventListener("click", (e) => addnextMove(e));
+document.getElementById("new-game").addEventListener("click", resetGame);
+document.getElementById("new-players").addEventListener("click", getUsers);
+
+
+
+var addnextMove = function(e) {
+  if (e.target.textContent === '' && !game.gameOver) {
     // Get the id of the cell clicked
     // Parse it into ['r', row, 'c', column]
     let parsedId = e.target.id.split('');
-    // Add nextMove to gameState[row][column]
-    gameState[parsedId[1]][parsedId[3]] = nextMove;
+    // Add game.nextMove to game.board[row][column]
+    game.board[parsedId[1]][parsedId[3]] = game.nextMove;
     // Re-render the board
     renderBoard();
     // Check to see if someone won
     checkBoard();
-    // update nextMove to be the following players move
-    if (nextMove === 'X') {
-      nextMove = 'O';
+    // update game.nextMove to be the following players move
+    if (game.nextMove === 'X') {
+      game.nextMove = 'O';
     } else {
-      nextMove = 'X';
+      game.nextMove = 'X';
     }
-  } else if (gameOver) {
+  } else if (game.gameOver) {
     alert('This game is over... better luck next time! Clear the board to try again.')
   }else {
     alert('Oops! Looks like that box is already taken. Please try again!');
   }
 }
 
+var getUsers = function () {
 
+}
