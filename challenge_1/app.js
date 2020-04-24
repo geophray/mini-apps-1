eeab6c0b3f17c01632418d3c69e3e6351771d4b5
Jeff Maughan <geophray@gmail.com>
2020-04-24 11:44:13 -0700

    Add function for rendering board state

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,7 +3,21 @@ console.log('hello');
 //=================================================================
 // View - handles delivering output to client
 //=================================================================
+var renderBoard = function () {
+  // Output the current state of the game to the board.
+  for (let i = 0; i < gameState.length; i++) {
+    let currentRow = gameState[i];
+    for (let j = 0; j < currentRow.length; j++) {
+      document.getElementById('r' + i + 'c' + j).innerText = currentRow[j];
+    }
+  }
+}
 
+//=================================================================
+// Model - handles the state of the game/board
+//=================================================================
+var gameState;
+var nextMove;
 
 
 
