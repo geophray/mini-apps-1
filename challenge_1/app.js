3551b4d63f01d8bdc793ed3aab1dfdd9c32d0da8
Jeff Maughan <geophray@gmail.com>
2020-04-24 12:01:15 -0700

    Add basic game functionality.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -25,6 +25,10 @@ var nextMove;
 // Controller - handles the game logic
 //=================================================================
 
+// Add event listeners
+document.getElementById("board").addEventListener("click", (e) => addNextMove(e));
+document.getElementById("reset").addEventListener("click", (e) => resetGame());
+
 // Function to reset the gameboard
 var resetGame = function () {
   gameState = [
@@ -39,12 +43,27 @@ var resetGame = function () {
 // Initialize an empty board
 resetGame();
 
-document.getElementById("board").addEventListener('click', (e) => addNextMove(e));
 
 var addNextMove = function(e) {
   console.log(e);
-  // if ()
-  // debugger;
+  if (e.target.textContent === '') {
+    // Get the id of the cell clicked
+    // Parse it into ['r', row, 'c', column]
+    let parsedId = e.target.id.split('');
+    // Add nextMove to gameState[row][column]
+    gameState[parsedId[1]][parsedId[3]] = nextMove;
+    // Re-render the board
+    renderBoard();
+    // update nextMove to be the following players move
+    if (nextMove === 'X') {
+      nextMove = 'O';
+    } else {
+      nextMove = 'X';
+    }
+  } else {
+    alert('Oops! Looks like that box is already taken. Please try again!');
+  }
+
 }
 
 
