789e711e7a0c2368c0b889c01538dc8b8b509269
Jeff Maughan <geophray@gmail.com>
2020-04-25 11:29:14 -0700

    Add basic server setup and serve public directory on port 1235

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('public'));
+
+
+const port = process.env.PORT || 1235;
+app.listen(port, () => {
+  console.log(`Server listening on http://127.0.0.1:${port}`);
+})
 
