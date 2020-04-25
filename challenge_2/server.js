f8c425e535d2a94742b8503f12576eeafde9f5dc
Jeff Maughan <geophray@gmail.com>
2020-04-24 19:06:15 -0700

    Set up express server and have it listen on localhost:3000

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.static(path.resolve(__dirname, './client')));
 
+app.listen(port, () => {
+  console.log(`App listening on http://127.0.0.1:${port}`);
+})
