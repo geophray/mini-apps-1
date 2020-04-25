45e6e79064922123ee2dac950f44b765d1b1f2c6
Jeff Maughan <geophray@gmail.com>
2020-04-25 11:26:06 -0700

    Add basic structure of app.jsx

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props){
+    super(props);
 
+  }
+
+  render() {
+    return (
+      <div>Hello World!  From my React.jsx.</div>
+    )
+  }
+
+}
+
+console.log('My react is being executed/transpiled!');
+
+ReactDOM.render(<App />, document.getElementById('app'));
