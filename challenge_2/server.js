33c94ccc51dd65e9592d28677d6fa5fc8c81a559
Jeff Maughan <geophray@gmail.com>
2020-04-24 19:58:50 -0700

    Send form data to api endpoint successfully

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, './client')));
 
+// app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/api/json', (req, res) => {
+  console.log('Here is the body of the doc: ', req.body);
+  res.status(200).send("Success!");
+})
+
 app.listen(port, () => {
   console.log(`App listening on http://127.0.0.1:${port}`);
 })
